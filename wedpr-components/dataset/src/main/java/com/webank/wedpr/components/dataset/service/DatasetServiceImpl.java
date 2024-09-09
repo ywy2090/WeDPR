6fe4d97c8a4a56bafba609d5b1846f52b4f29163
octopus <912554887@qq.com>
2024-09-09 14:41:53 +0800

    fix unsuccess dataset visible for others bug

--- wedpr-components/dataset/src/main/java/com/webank/wedpr/components/dataset/service/DatasetServiceImpl.java
+++ wedpr-components/dataset/src/main/java/com/webank/wedpr/components/dataset/service/DatasetServiceImpl.java
@@ -522,6 +522,7 @@ public class DatasetServiceImpl implements DatasetServiceApi {
 
                 List<Dataset> datasetList =
                         datasetMapper.queryVisibleDatasetsForUser(
+                                user,
                                 agency,
                                 userSubject,
                                 userGroupSubjectList,
