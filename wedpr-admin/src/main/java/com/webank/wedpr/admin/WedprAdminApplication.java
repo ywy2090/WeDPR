bf395529d7e7887c1ed66b25f44a13b9eedaad59
caryliao(廖飞强) <caryliao@webank.com>
2024-10-10 15:00:41 +0800

    add sync for admin

--- wedpr-admin/src/main/java/com/webank/wedpr/admin/WedprAdminApplication.java
+++ wedpr-admin/src/main/java/com/webank/wedpr/admin/WedprAdminApplication.java
@@ -1,16 +1,31 @@
 package com.webank.wedpr.admin;
 
 import com.webank.wedpr.components.initializer.WeDPRApplication;
+import com.webank.wedpr.components.leader.election.LeaderElection;
+import com.webank.wedpr.components.sync.ResourceSyncer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.webank"})
+@Slf4j
 public class WedprAdminApplication {
 
     public static void main(String[] args) throws Exception {
         long startT = System.currentTimeMillis();
         WeDPRApplication.main(args, "WeDPR-ADMIN");
+        ResourceSyncer resourceSyncer =
+                WeDPRApplication.getApplicationContext().getBean(ResourceSyncer.class);
+        log.info("start resourceSyncer");
+        resourceSyncer.start();
+        log.info("start resourceSyncer success");
+        // Note: must start leaderElection after the resourceSyncer started
+        LeaderElection leaderElection =
+                WeDPRApplication.getApplicationContext().getBean(LeaderElection.class);
+        log.info("start leaderElection");
+        leaderElection.start();
+        log.info("start leaderElection success");
         System.out.println(
                 "WeDPR-ADMIN: start WedprAdminApplication success, timecost: "
                         + (System.currentTimeMillis() - startT)
